99262f21dd1d80f1bb3d99b100f08cd63e701b66
fenglianshuai <1351716984@qq.com>
2021-07-03 02:07:19 +0800

    fix: 添加md文件

--- one/server.js
+++ one/server.js
@@ -4,11 +4,9 @@ http.createServer((req, res) => {
     let body = [];
     req.on('error', (err) => {
         console.error(err)
-    })
-    req.on('data', (chunk) => {
+    }).on('data', (chunk) => {
         body.push(chunk)
-    })
-    req.on('end', () => {
+    }).on('end', () => {
         body = Buffer.concat(body).toString();
         console.log('body:', body);
         res.writeHead(200, { 'Content-Type': 'text/html;' })
